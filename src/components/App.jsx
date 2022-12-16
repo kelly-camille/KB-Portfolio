2b989c5f1e6ba02b4047246c27db120a12e2864e
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-12-16 13:50:42 -0800

    reorganize files and assets

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,9 @@
-// old code
+import React from react;
 
+const App = () => {
+  return(
+    <div>App</div>
+  )
+}
 
-
-// import Main from '../components/main.jsx';
-// import React from 'react';
-// import '../../../css/normali'
-// import{Route, Routes} from 'react-router-dom';
-
- 
-// function App() {
-//   return (
-//     <React.Fragment>
-//   <Routes>
-//     <Route path='/' element={<Main/>} />
-//   </Routes>
-//    </React.Fragment>
-//   );
-// }
-
-// export default App;
+export default App;
