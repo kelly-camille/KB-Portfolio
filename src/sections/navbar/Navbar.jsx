39f709b48d1da32f6b71f7cda0f7303ce158950d
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-12-17 13:49:43 -0800

    create navbar links

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,8 +1,21 @@
+import logo from '../../assets/logo.png'
+import data from './data'
 import './navbar.css'
+
 
 const Navbar = () => {
   return (
-    <nav><h2>Nav</h2></nav>
+    <nav>
+        <div className="container nav__container"></div>
+        <a href=""></a>
+        <img src={logo} alt="logo" />
+        <ul className='nav__menu'>
+          {
+            data.map(item=> <li key={item.id}><a href={item.link}>{item.title}</a></li>)
+          }
+        </ul>
+        <button></button>
+    </nav>
   )
 }
 
