01170d023b65c21406413df278982451b678df5c
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-12-19 21:57:03 -0800

    projects photo css wip

--- src/sections/portfolio/Portfolio.jsx
+++ src/sections/portfolio/Portfolio.jsx
@@ -2,12 +2,13 @@ import'./portfolio.css'
 import Projects from './Projects';
 import ProjectsCategories from './ProjectsCategories';
 import data from './data'
-import{useSate, useState} from 'react'
+import{ useState} from 'react'
 
 const Portfolio = () => {
   const [projects, setProjects] = useState(data);
   return (
-    <section id="portfolio"><h2>Recent Projects</h2>
+    <section id="portfolio">
+    <h2>Recent Projects</h2>
     <p>Check out some of the Projects I've built recently.</p>
     <div className="container portfolio__container">
       <ProjectsCategories/>
