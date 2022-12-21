b0e0b91c6447744b10d5814c34a406818813b26b
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-12-21 10:23:00 -0800

    wip editing skills section

--- src/sections/services/data.js
+++ src/sections/services/data.js
@@ -6,18 +6,31 @@ import {AiFillAppstore} from 'react-icons/ai'
 
 const data = [
     {
-        id: 1, icon: <SiJavascript/>, title: 'JavaScript', desc: "My designs are modern and intuitive. I use industry-standard rules to make sure your users have fun using your product."
+        id: 1, icon: <SiJavascript/>, title: 'JavaScript', desc: "Object Oriented and Functional Programming"
     },
     {
-        id: 2, icon: <RiReactjsLine/>, title: 'Frontend Development', desc: "Your product will look good and will be accessible on all devices, including mobile phones, tablets, and desktop."
+        id: 2, icon: <RiReactjsLine/>, title: 'React', desc: "Your product will look good and will be accessible on all devices, including mobile phones, tablets, and desktop."
     },
     {
         id: 3, icon: <FaServer/>, title: 'C# / .NET', desc: "The security of your business/product is taken seriously right from the start of the project. I will make sure your website/app is secure from attacks."
     },
     {
-        id: 4, icon: <AiFillAppstore/>, title: 'App Development', desc: 'Unlike other developers in the market, I will building an app that runs on both IOS and Android devices without any extra cost to you.'
+        id: 4, icon: <AiFillAppstore/>, title: 'APIs / SQL', desc: 'Unlike other developers in the market, I will building an app that runs on both IOS and Android devices without any extra cost to you.'
     }
 ]
 
 
 export default data
+
+// SKILLS
+// ● JavaScript
+// ● C#/.NET
+// ● HTML/CSS
+// ● SQL
+// ● MVC
+// ● Object Oriented
+// ● Test Driven Development
+// ● Webpack
+// ● Pair Programming
+// ● Sales/Marketing
+// ● CRM Management
